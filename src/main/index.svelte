f43db185e3e5ec6c46dd276676f4202c8d23d81b
byj0101 <byj010101@gmail.com>
2022-05-19 01:06:30 +0900

    eslint, prettier 적용

--- src/main/index.svelte
+++ src/main/index.svelte
@@ -1,42 +1,41 @@
 <script lang="ts">
-    const logoImage: string = "image/group-logo.png"
+  const logoImage: string = "image/group-logo.png";
 </script>
 
-
-    <div class="header" id="header">
-        <div class="header-container">
-        <div class="main-logo">
-            <div><img src={logoImage} alt="logo"/></div>
-        </div>
-        <div class="category">
-            <div>마이페이지</div>
-            <div><a href="./sign-in.html" title="로그인">로그인</a></div>
-            <div>나이트모드</div>
-        </div>
-        </div>
+<div class="header" id="header">
+  <div class="header-container">
+    <div class="main-logo">
+      <div><img src={logoImage} alt="logo" /></div>
     </div>
-    <div class="main-container">
-        <div class="container">
-        <div class="word">
-            <div class="word-container">
-            <div>
-                <div>좋은 디자인은</div>
-                <div>
-                <div>불필요한 관심을 끌지 않는다.</div>
-                </div>
-            </div>
-            <div class="dieter-rams-img">
-                <img src="./image/dieter_rams_first.png" alt="" />
-                <img src="./image/dieter_rams_sec.png" alt="" />
-                <div>
-                <div class="important">난 중요해 🔥</div>
-                </div>
-                <div>
-                <div class="important reverse">난 없어도 돼</div>
-                </div>
-            </div>
-            </div>
+    <div class="category">
+      <div>마이페이지</div>
+      <div><a href="./sign-in.html" title="로그인">로그인</a></div>
+      <div>나이트모드</div>
+    </div>
+  </div>
+</div>
+<div class="main-container">
+  <div class="container">
+    <div class="word">
+      <div class="word-container">
+        <div>
+          <div>좋은 디자인은</div>
+          <div>
+            <div>불필요한 관심을 끌지 않는다.</div>
+          </div>
         </div>
+        <div class="dieter-rams-img">
+          <img src="./image/dieter_rams_first.png" alt="" />
+          <img src="./image/dieter_rams_sec.png" alt="" />
+          <div>
+            <div class="important">난 중요해 🔥</div>
+          </div>
+          <div>
+            <div class="important reverse">난 없어도 돼</div>
+          </div>
         </div>
-    <div class="for-height"></div>
+      </div>
+    </div>
+  </div>
+  <div class="for-height" />
 </div>
--- src/signIn/index.svelte
+++ src/signIn/index.svelte
@@ -1,64 +1,65 @@
 <script lang="ts">
-    const logoImage: string = "image/group-logo.png"
-    const googleLogoImage: string = "image/google_logo.png"
-    const naverLogoImage: string = "image/naver_logo.png"
-    const kakaoLogoImage: string = "image/kakao_logo.png"
-
+  const logoImage: string = "image/group-logo.png";
+  const googleLogoImage: string = "image/google_logo.png";
+  const naverLogoImage: string = "image/naver_logo.png";
+  const kakaoLogoImage: string = "image/kakao_logo.png";
 </script>
-    <div class="sign-in-container">
-      <div class="sub-container">
-        <div class="logo-sign-in">
-            <a href="./index.html" title="로그인">
-              <img src={logoImage} alt="logo"/>
-            </a>
+
+<div class="sign-in-container">
+  <div class="sub-container">
+    <div class="logo-sign-in">
+      <a href="./index.html" title="로그인">
+        <img src={logoImage} alt="logo" />
+      </a>
+    </div>
+
+    <div class="sign-in">
+      <div class="sign-in-input">
+        <div class="sub-title">아이디</div>
+        <div>
+          <input type="email" placeholder="이메일을 입력해주세요." />
         </div>
+      </div>
 
-        <div class="sign-in">
+      <div class="sign-in-input">
+        <div><div class="sub-title">비밀번호</div></div>
+        <div>
+          <input type="email" placeholder="이메일을 입력해주세요." />
+        </div>
+      </div>
 
-          <div class="sign-in-input">
-            <div class="sub-title">아이디</div>
-            <div><input type="email" placeholder="이메일을 입력해주세요." /></div>
-          </div>
+      <div class="button-regular">
+        <div>로그인</div>
+      </div>
 
-          <div class="sign-in-input">
-            <div><div class="sub-title">비밀번호</div></div>
-            <div><input type="email" placeholder="이메일을 입력해주세요." /></div>
-          </div>
+      <div class="another-sign-up-info">
+        <div class="line" />
+        <div class="info">다른 계정으로 로그인</div>
+        <div class="line" />
+      </div>
 
-          <div class="button-regular">
-              <div>로그인</div>
-          </div>
-
-          <div class="another-sign-up-info">
-            <div class="line"></div>
-            <div class="info">다른 계정으로 로그인</div>
-            <div class="line"></div>
-          </div>
-
-          <div class="another-sign-up">
-            <div class="another-logo-container">
-              <div class="container">
-                <div class="another-logo google">
-                  <img src={googleLogoImage} alt="google">
-                </div>
-                <div class="another-text">Google</div>
-              </div>
-              <div class="container">
-                <div class="another-logo naver">
-                    <img src={naverLogoImage} alt="naver">
-                </div>
-                <div class="another-text">Naver</div>
-              </div>
-              <div class="container">
-                <div class="another-logo kakao">
-                    <img src={kakaoLogoImage} alt="kako">
-                </div>
-                <div class="another-text">Kakao</div>
-              </div>
+      <div class="another-sign-up">
+        <div class="another-logo-container">
+          <div class="container">
+            <div class="another-logo google">
+              <img src={googleLogoImage} alt="google" />
             </div>
+            <div class="another-text">Google</div>
           </div>
-
+          <div class="container">
+            <div class="another-logo naver">
+              <img src={naverLogoImage} alt="naver" />
+            </div>
+            <div class="another-text">Naver</div>
+          </div>
+          <div class="container">
+            <div class="another-logo kakao">
+              <img src={kakaoLogoImage} alt="kako" />
+            </div>
+            <div class="another-text">Kakao</div>
+          </div>
         </div>
       </div>
-      </div>
-
+    </div>
+  </div>
+</div>
