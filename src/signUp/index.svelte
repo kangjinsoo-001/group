2ae8bd2230e9be0b86ad43948a3187fa945257fd
kang jin soo-001 <wlstnrkd124@gmail.com>
2022-06-02 19:18:06 +0900

    6월2일 작업사항

--- src/signUp/index.svelte
+++ src/signUp/index.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-  //   import { Link } from "svelte-navigator";
+  import { Link } from "svelte-navigator";
 
-  //   const logoImage: string = "image/group-logo.png";
+  const logoImage: string = "image/group_logo_gray.png";
 </script>
 
 <div class="sign-in-container">
   <div class="sub-container">
-    <div class="sign-up-container">test</div>
+    <div class="logo-sign-up">
+      <Link to="/">
+        <img src={logoImage} alt="logo" />
+      </Link>
+    </div>
+    <div class="sign-up-container">
+      <div class="sign-up-sec-info">회원가입</div>
+      <div class="sign-up-info">연동할 이메일을</div>
+      <div class="sign-up-info">입력해주세요.</div>
+      <div class="sign-in-input">
+        <div class="sign-up-email-input">
+          <input type="email" placeholder="이메일을 입력해주세요." />
+          <input type="email" placeholder="이메일을 입력해주세요." />
+        </div>
+      </div>
+    </div>
   </div>
 </div>
