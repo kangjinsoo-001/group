b34b5139e7f7c5efc6bffe424d51b28aab0e554e
kang jin soo-001 <wlstnrkd124@gmail.com>
2022-06-02 20:01:57 +0900

    6/2 이메일 회원가입 퍼블리싱 2차

--- src/signUp/index.svelte
+++ src/signUp/index.svelte
@@ -13,13 +13,23 @@
     </div>
     <div class="sign-up-container">
       <div class="sign-up-sec-info">회원가입</div>
-      <div class="sign-up-info">연동할 이메일을</div>
-      <div class="sign-up-info">입력해주세요.</div>
+      <div class="sign-up-info-container">
+        <div class="info">연동할 이메일을</div>
+        <div class="info sec">입력해주세요.</div>
+      </div>
       <div class="sign-in-input">
         <div class="sign-up-email-input">
-          <input type="email" placeholder="이메일을 입력해주세요." />
+          <input
+            class="mr10"
+            type="email"
+            placeholder="이메일을 입력해주세요."
+          />
           <input type="email" placeholder="이메일을 입력해주세요." />
         </div>
+      </div>
+      <div class="flc jsb">
+        <div class="go-back-btn">뒤로가기</div>
+        <div class="basic-btn">다음</div>
       </div>
     </div>
   </div>
