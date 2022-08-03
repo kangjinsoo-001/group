d657883f3ef2c96e8f56e41f79302ccfbc39dee9
byj0101 <byj010101@gmail.com>
2022-08-03 21:53:44 +0900

    메인 레이아웃 적용

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import Ranking from "$lib/components/Ranking.svelte";
+	import { Col, Container, Row } from "sveltestrap";
 </script>
 
 <svelte:head>
@@ -11,50 +12,6 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<Container>
+	<Row><Col xs="7">1</Col><Col xs="5"><Ranking /></Col></Row>
+</Container>
