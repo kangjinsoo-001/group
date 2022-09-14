ad79011bba5d0ccb6928e22efe6e54084f3c5d07
kang jin soo-001 <wlstnrkd124@gmail.com>
2022-09-14 20:48:59 +0900

    다크 모드 관련 퍼블리싱 1차

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -59,11 +59,15 @@
 	}
 
 	:global(body) {
-		background-color: #f2eee2;
+		background-color: #ffffff;
 		color: #0084f6;
 		transition: background-color 0.3s;
 	}
 	:global(body.dark-mode) {
+		background-color: #1d3040;
+		color: #bfc2c7;
+	}
+	:global(.dark-mode) {
 		background-color: #1d3040;
 		color: #bfc2c7;
 	}
